509d546527890c35a2deebda160d87c2e4492b6c
José María Sosa Resendiz <jose.skedar@gmail.com>
2024-01-17 10:14:35 -0600

    adding multi transfer for erc20 and eth

--- src/MultiTransfer.sol
+++ src/MultiTransfer.sol
@@ -11,22 +11,50 @@ struct Transfer {
 
 contract MultiTransfer is ReentrancyGuard {
 
+    /// From a best practices standpoint, especially in financial transactions,
+    /// ensuring that all prerequisites are met before performing any state-changing
+    /// actions (like transferring Ether) is often considered safer and more reliable.
+
+    function multiNativeTransfer(
+        Transfer[] calldata _transfers
+    ) external payable nonReentrant {
+        require(msg.value > 0, "No Ether sent");
+
+        uint256 accumAmount;
+        uint256 len = _transfers.length;
+        for (uint i = 0; i < len; ++i) {
+            accumAmount += _transfers[i].amount;
+        }
+        require(msg.value >= accumAmount, "Insufficient amount");
+
+        for (uint i = 0; i < len; ++i) {
+            require(payable(_transfers[i].receiver_id).send(_transfers[i].amount), "Failed to send Ether");
+        }
+
+        // Returning the change 🪙
+        if (accumAmount < msg.value) {
+            require(payable(msg.sender).send(msg.value - accumAmount), "Transfer failed");
+        }
+    }
+
     function multiTransfer(
         IERC20 _token,
         uint256 _amount,
         Transfer[] calldata _transfers
     ) external nonReentrant {
+        require(_amount > 0, "No Amount sent");
         _token.transferFrom(msg.sender, address(this), _amount);
 
-        uint256 _txAmount;
         uint256 accumAmount;
         uint256 len = _transfers.length;
         for (uint i = 0; i < len; ++i) {
-            _txAmount = _transfers[i].amount;
-            accumAmount += _txAmount;
-            require(_token.transfer(_transfers[i].receiver_id, _txAmount), "Transfer failed");
+            accumAmount += _transfers[i].amount;
         }
         require(_amount >= accumAmount, "Insufficient amount");
+
+        for (uint i = 0; i < len; ++i) {
+            require(_token.transfer(_transfers[i].receiver_id, _transfers[i].amount), "Transfer failed");
+        }
 
         // Returning the change 🪙
         if (accumAmount < _amount) {
