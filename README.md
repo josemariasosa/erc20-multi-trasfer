f6ddf6be9fdacdb89f826e8d869399fca7a39fe9
José María Sosa Resendiz <jose.skedar@gmail.com>
2024-01-18 10:55:01 -0600

    new version and releases

---
+++ README.md
@@ -0,0 +1,6 @@
+# ERC20 Multi Transfer
+
+## Deployed to Networks
+
+- q testnet https://explorer.qtestnet.org/address/0x11adbe0E9Ca21DBA3068381E750d93BAc92323aD
+- q mainnet https://explorer.q.org/address/0x23B4904924cFF879DE6d084B8AA3b40F09A7A150
--- src/MultiTransfer.sol
+++ src/MultiTransfer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { ReentrancyGuard } from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
@@ -8,6 +8,7 @@ struct Transfer {
     address receiver_id;
     uint256 amount;
 }
+
 
 contract MultiTransfer is ReentrancyGuard {
 
@@ -39,26 +40,16 @@ contract MultiTransfer is ReentrancyGuard {
 
     function multiTransfer(
         IERC20 _token,
-        uint256 _amount,
         Transfer[] calldata _transfers
     ) external nonReentrant {
-        require(_amount > 0, "No Amount sent");
-        _token.transferFrom(msg.sender, address(this), _amount);
-
         uint256 accumAmount;
         uint256 len = _transfers.length;
-        for (uint i = 0; i < len; ++i) {
-            accumAmount += _transfers[i].amount;
-        }
-        require(_amount >= accumAmount, "Insufficient amount");
+        for (uint i = 0; i < len; ++i) { accumAmount += _transfers[i].amount; }
+        require(accumAmount > 0, "No Amount sent");
+        require(_token.transferFrom(msg.sender, address(this), accumAmount), "Not approved");
 
         for (uint i = 0; i < len; ++i) {
             require(_token.transfer(_transfers[i].receiver_id, _transfers[i].amount), "Transfer failed");
-        }
-
-        // Returning the change 🪙
-        if (accumAmount < _amount) {
-            require(_token.transfer(msg.sender, _amount - accumAmount), "Transfer failed");
         }
     }
 }
